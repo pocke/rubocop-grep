3a3f07b86cade01756975af1f7dcbd2da0f0be3d
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2023-02-27 00:44:27 +0900

    Pass steep

--- lib/rubocop/grep.rb
+++ lib/rubocop/grep.rb
@@ -4,9 +4,8 @@ require_relative "grep/version"
 
 module RuboCop
   module Grep
-    class Error < StandardError; end
     # Your code goes here...
-    PROJECT_ROOT   = Pathname.new(__dir__).parent.parent.expand_path.freeze
+    PROJECT_ROOT   = Pathname.new(__dir__ || raise).parent.parent.expand_path.freeze
     CONFIG_DEFAULT = PROJECT_ROOT.join('config', 'default.yml').freeze
     CONFIG         = YAML.safe_load(CONFIG_DEFAULT.read).freeze
 
